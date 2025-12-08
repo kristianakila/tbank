44ddb35e05b2813a8917907f1b2d3a53a6b1ca37
kristianakila <kristianakila@ya.ru>
2025-12-08 13:51:06 +0300

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,22 @@
+// models/Product.js
+const productsData = require('../src/data/prognosesData.json');
+
+class Product {
+  static async getAllProducts() {
+    return productsData;
+  }
+
+  static async getProductById(id) {
+    return productsData.find(product => product.id === id);
+  }
+
+  static async validateProduct(productId) {
+    const product = await this.getProductById(productId);
+    if (!product) {
+      throw new Error(`Товар с ID ${productId} не найден`);
+    }
+    return product;
+  }
+}
+
+module.exports = Product;
