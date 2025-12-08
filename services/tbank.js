cfb1dadbd9dd44e31575c849d8e2f4d9b17d1089
kristianakila <kristianakila@ya.ru>
2025-12-08 11:13:18 +0300

    Delete services directory

--- services/tbank.js
+++
@@ -1,9 +0,0 @@
-const TbankPayments = require('tbank-payments');
-
-const tbank = new TbankPayments({
-  merchantId: process.env.TBANK_MERCHANT_ID,
-  secret: process.env.TBANK_SECRET,
-  apiUrl: process.env.TBANK_API_URL
-});
-
-module.exports = tbank;
